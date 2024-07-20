5c700ff82714d1e4ed0d64f43b2ace95253609d3
Aadiaks22 <adityaaks220@gmail.com>
2024-07-20 17:03:13 +0530

    All API created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
 
 const JWT_SECRET = 'ADITYAISAGOODB$OY';
 
-// Create a user using: POST "/api/auth/createuser". Doesn't require Auth
+//Route 1: Create a user using: POST "/api/auth/createuser". Doesn't require Auth
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid Email').isEmail(),
@@ -50,5 +51,56 @@ router.post('/createuser', [
         res.status(500).send('Server Error');
     }
 });
+
+//Route 2: Authenticate a user using: POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password Cannot be blank').exists(),
+], async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+    try {
+        // Check if a user with the same email already exists
+        let user = await User.findOne({ email});
+        if (!user) {
+            return res.status(400).json({ error: "Please Enter Correct Credentials" });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if(!passwordCompare){
+            return res.status(400).json({ error: "Please Enter Correct Credentials" });
+        }
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({authToken});
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//Route 3: Get Loggedin user details using: POST "/api/auth/getuser". login required
+router.post('/getuser',fetchuser, async (req, res) => {
+    // Check for validation errors
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
